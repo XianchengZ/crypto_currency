08c7bcb479057fd6f2b6df7eea1be18570d7ec98
Andrew Zang <andrewzang@Andrews-MacBook-Pro.local>
2021-06-25 19:14:43 -0400

    block component

---
+++ crypto-hash.test.js
@@ -0,0 +1,15 @@
+const cryptoHash = require("./crypto-hash");
+
+describe("cryptoHash()", () => {
+  it("generates a SHA-256 hashed output", () => {
+    expect(cryptoHash("hello")).toEqual(
+      "2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824"
+    );
+  });
+
+  it("produces the same hash with the same input arguements in any order", () => {
+    expect(cryptoHash("one", "two", "three")).toEqual(
+      cryptoHash("three", "two", "one")
+    );
+  });
+});
